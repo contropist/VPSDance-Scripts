7de5f8356383dc5f7c72aa2200dadb4be08b597f
cloudend <cloudend@gmail.com>
2022-03-07 12:36:22 +0800

    Update and rename superBench to superBench.sh

--- superBench.sh
+++ superBench.sh
@@ -4,5 +4,7 @@
 
 bash <(
   curl -Lso- https://raw.githubusercontent.com/oooldking/script/master/superbench_git.sh \
+  | sed 's/^.*fast\.com.*$/:;/I' \
+  | sed 's/^.*[ /]fast_com.*.py.*$/#/I' \
   | sed '/^[ \t]*speed_fast_com$/d'
 )
