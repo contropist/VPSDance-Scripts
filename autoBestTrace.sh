d708d3d5d23fd2d0568df093e2a589e36fdf65fd
cloudend <cloudend@gmail.com>
2022-03-07 12:40:58 +0800

    Create autoBestTrace.sh

---
+++ autoBestTrace.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# https://github.com/nyjx/autoBestTrace
+# https://github.com/flyzy2005/shell
+
+
+bash <(
+  curl -Lso- https://raw.githubusercontent.com/nyjx/autoBestTrace/main/autoBestTrace.sh
+)
