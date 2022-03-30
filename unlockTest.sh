cf0aed0fc26948971c1c50c6dc0bd7902a2f5890
cloudend <cloudend@gmail.com>
2022-03-30 10:22:33 +0800

    Update unlockTest.sh

--- unlockTest.sh
+++ unlockTest.sh
@@ -3,12 +3,14 @@
 # bash <(curl -L -s check.unlock.media)
 # https://github.com/lmc999/RegionRestrictionCheck
 
-bash <(
-  curl -L -s check.unlock.media \
-  | sed '/^[ \t]*echo\( -e\)\? "[-]*"\(.*\)\?$/d' \
-  | sed '/^[ \t]*echo\( -e\)\? "[=]*"\(.*\)\?$/d' \
-  | sed 's/ CheckV6().*$/&\n printf "%-39s\\n" \| sed "s\/\\s\/-\/g"/' \
-  | sed 's/ Goodbye().*$/&\n printf "%-39s\\n" \| sed "s\/\\s\/-\/g"/' \
-  | sed '/echo\( -e\)\? ""\(.*\)\?$/d'
-)
-
+main() {
+  bash <(
+    curl -L -s check.unlock.media \
+    | sed '/^[ \t]*echo\( -e\)\? "[-]*"\(.*\)\?$/d' \
+    | sed '/^[ \t]*echo\( -e\)\? "[=]*"\(.*\)\?$/d' \
+    | sed 's/ CheckV6().*$/&\n printf "%-39s\\n" \| sed "s\/\\s\/-\/g"/' \
+    | sed 's/ Goodbye().*$/&\n printf "%-39s\\n" \| sed "s\/\\s\/-\/g"/' \
+    | sed '/echo\( -e\)\? ""\(.*\)\?$/d'
+  )
+}
+main
