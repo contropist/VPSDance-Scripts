c286f358c3abafb5f930f6c5c3139b978bad1722
cloudend <cloudend@gmail.com>
2022-03-30 10:21:34 +0800

    Update bench.sh

--- bench.sh
+++ bench.sh
@@ -2,16 +2,19 @@
 
 # https://github.com/teddysun/across/blob/master/bench.sh
 
-bash <(
-  curl -Lso- https://raw.githubusercontent.com/teddysun/across/master/bench.sh \
-  | sed "/Los Angeles/Ii speed_test '34115' 'TianJin CT'" \
-  | sed "/Los Angeles/Ii speed_test '27594' 'Guangzhou CT'" \
-  | sed "/Los Angeles/Ii speed_test '4870' 'Changsha CU'" \
-  | sed "/Los Angeles/Ii speed_test '13704' 'Nanjing CU'" \
-  | sed "/Los Angeles/Ii speed_test '26404' 'Hefei CM'" \
-  | sed "/Los Angeles/Ii speed_test '15863' 'Nanning CM'" \
-  | sed "/.*, CN/Id"
-)
+main() {
+  bash <(
+    curl -Lso- https://raw.githubusercontent.com/teddysun/across/master/bench.sh \
+    | sed "/Los Angeles/Ii speed_test '34115' 'TianJin CT'" \
+    | sed "/Los Angeles/Ii speed_test '27594' 'Guangzhou CT'" \
+    | sed "/Los Angeles/Ii speed_test '4870' 'Changsha CU'" \
+    | sed "/Los Angeles/Ii speed_test '13704' 'Nanjing CU'" \
+    | sed "/Los Angeles/Ii speed_test '26404' 'Hefei CM'" \
+    | sed "/Los Angeles/Ii speed_test '15863' 'Nanning CM'" \
+    | sed "/.*, CN/Id"
+  )
+}
+main
 
 # https://www.speedtest.net/api/js/servers?search=China%20Telecom
 # https://www.speedtest.net/api/js/servers?search=电信
