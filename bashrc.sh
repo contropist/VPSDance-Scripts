5596082db5f4d50c0e31c052e970267116b936ee
cloudend <cloudend@gmail.com>
2022-03-30 21:02:31 +0800

    Update bashrc.sh

--- bashrc.sh
+++ bashrc.sh
@@ -12,7 +12,7 @@ for file in /root/.bashrc /home/*/.bashrc ; do
   sed -i '/^# => vps.dance/,/^# <= vps.dance/d' $file;
   # insert lines
   printf "%s\n" "# => vps.dance" >> $file;
-  printf "%s\n" "$(curl -Lso- jsonip.com)" >> $file;
+  printf "%s\n" "$(curl -Lso- https://raw.githubusercontent.com/VPSDance/scripts/main/files/bashrc)" >> $file;
   printf "%s\n" "# <= vps.dance" >>$file;
 done
 source ~/.bashrc
