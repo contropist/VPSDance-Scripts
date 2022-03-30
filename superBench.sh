8546a3df1af180986aa71920878def478423e8f3
cloudend <cloudend@gmail.com>
2022-03-30 09:11:04 +0800

    Update superBench.sh

--- superBench.sh
+++ superBench.sh
@@ -2,19 +2,39 @@
 
 # https://github.com/oooldking/script
 
-bash <(
-  curl -Lso- https://raw.githubusercontent.com/oooldking/script/master/superbench_git.sh \
-  | sed 's/^.*fast\.com.*$/:;/I' \
-  | sed 's/^.*[ /]fast_com.*.py.*$/#/I' \
-  | sed "s/27377' 'Beijing/34115' 'TianJin/Ig" \
-  | sed "s/27154' 'TianJin/4870' 'Changsha/Ig" \
-  | sed "s/26678' 'Guangzhou 5G/13704' 'Nanjing/Ig" \
-  | sed "s/17184' 'Tianjin/26404' 'Hefei/Ig" \
-  | sed "/.*26850' 'Wuxi/Id" \
-  | sed "s/27249' 'Nanjing 5G/15863' 'Nanning/Ig" \
-  | sed "/.*28491' 'Changsha/Id" \
-  | sed '/^[ \t]*speed_fast_com$/d'
-)
+ver_lte() { # <=
+  [  "$(printf '%s\n' "$@" | sort -V | head -n 1)" = "$1" ] && return 0 || return 1
+}
+python_version() {
+  python -V 2>&1 | awk '{print $2}' # | awk -F '.' '{print $1}'
+}
+main() {
+  # fix python3 error
+  local ver=$(python_version) # echo $ver
+  curl -Lso- https://cdn.jsdelivr.net/gh/oooldking/script@1.1.7/tools.py > tools.py
+  if (ver_lte 3 $ver); then
+    sed -i 's/print \(.*\)$/print(\1)/g' tools.py
+    sed -i 's/,urllib2,/,/g' tools.py
+    sed -i 's/,json,sys.*$/&\nimport urllib.request as urllib2/' tools.py
+    sed -i '/reload(sys).*$/d' tools.py
+    sed -i '/sys.setdefaultencoding.*$/d' tools.py
+    sed -i "s/para.encode('utf-8')/para/g" tools.py
+  fi
+  bash <(
+    curl -Lso- https://raw.githubusercontent.com/oooldking/script/master/superbench_git.sh \
+    | sed 's/^.*fast\.com.*$/:;/I' \
+    | sed 's/^.*[ /]fast_com.*.py.*$/#/I' \
+    | sed "s/27377' 'Beijing/34115' 'TianJin/Ig" \
+    | sed "s/27154' 'TianJin/4870' 'Changsha/Ig" \
+    | sed "s/26678' 'Guangzhou 5G/13704' 'Nanjing/Ig" \
+    | sed "s/17184' 'Tianjin/26404' 'Hefei/Ig" \
+    | sed "/.*26850' 'Wuxi/Id" \
+    | sed "s/27249' 'Nanjing 5G/15863' 'Nanning/Ig" \
+    | sed "/.*28491' 'Changsha/Id" \
+    | sed '/^[ \t]*speed_fast_com$/d'
+  )
+}
+main
 
 # https://www.speedtest.net/api/js/servers?search=China%20Telecom
 # https://www.speedtest.net/api/js/servers?search=电信
