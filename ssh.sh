88897d932c1f0cbe7ddf504ebcea597c558b7fb3
cloudend <cloudend@gmail.com>
2022-03-31 18:20:22 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 # Set up SSH public key authentication
+# bash <(curl -Lso- https://raw.githubusercontent.com/VPSDance/scripts/main/ssh.sh)
 
 mkdir -m 700 ~/.ssh; chmod 600 ~/.ssh/authorized_keys >> ~/.ssh/authorized_keys
 main () {
-  echo "Paste your SSH public key `~/.ssh/id_rsa.pub`:"
+  echo "Paste your SSH public key (~/.ssh/id_rsa.pub):"
   IFS= read -d '' -n 1 text
   while IFS= read -d '' -n 1 -t 2 c
   do
