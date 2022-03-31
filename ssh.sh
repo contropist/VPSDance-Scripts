eeba75fc6747dce818ad513e42697c05b12df47e
cloudend <cloudend@gmail.com>
2022-03-31 20:55:09 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -3,7 +3,12 @@
 # Set up SSH public key authentication
 # bash <(curl -Lso- https://raw.githubusercontent.com/VPSDance/scripts/main/ssh.sh)
 
-mkdir -m 700 ~/.ssh; chmod 600 ~/.ssh/authorized_keys >> ~/.ssh/authorized_keys
+# mkdir -m 700 ~/.ssh; chmod 600 ~/.ssh/authorized_keys >> ~/.ssh/authorized_keys
+
+mmkdir -p "${HOME}/.ssh"; chmod 700 "${HOME}/.ssh";
+if [[ ! -f "${HOME}/.ssh/authorized_keys" ]]; then echo '' >> ~/.ssh/authorized_keys; fi
+chmod 600 "${HOME}/.ssh/authorized_keys"
+
 main () {
   echo "Paste your SSH public key (~/.ssh/id_rsa.pub):"
   IFS= read -d '' -n 1 text
