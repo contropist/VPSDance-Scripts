e60bd65f5dee263fc006efc6be6872c406ee0af7
cloudend <cloudend@gmail.com>
2022-03-31 18:17:37 +0800

    Create ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Set up SSH public key authentication
+
+mkdir -m 700 ~/.ssh; chmod 600 ~/.ssh/authorized_keys >> ~/.ssh/authorized_keys
+main () {
+  echo "Paste your SSH public key `~/.ssh/id_rsa.pub`:"
+  IFS= read -d '' -n 1 text
+  while IFS= read -d '' -n 1 -t 2 c
+  do
+    text+=$c
+  done
+  echo "public key=$text"
+}
+main
