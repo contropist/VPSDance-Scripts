969da647cfb369cd2bc87489f9d612e2b064a4fc
cloudend <cloudend@gmail.com>
2022-04-01 12:46:07 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -5,9 +5,14 @@
 
 # mkdir -m 700 ~/.ssh; chmod 600 ~/.ssh/authorized_keys >> ~/.ssh/authorized_keys
 
-mkdir -p "${HOME}/.ssh"; chmod 700 "${HOME}/.ssh";
-if [[ ! -f "${HOME}/.ssh/authorized_keys" ]]; then echo '' >> ~/.ssh/authorized_keys; fi
-chmod 600 "${HOME}/.ssh/authorized_keys"
+
+for home in /root /home/*; # do { if [[ ! -e "$home" ]]; then continue; fi; echo $home; }; done
+do
+  if [[ ! -e "$home" ]]; then continue; fi;
+  mkdir -p "${home}/.ssh"; chmod 700 "${home}/.ssh";
+  if [[ ! -f "${home}/.ssh/authorized_keys" ]]; then echo '' >> ~/.ssh/authorized_keys; fi
+  chmod 600 "${home}/.ssh/authorized_keys"
+done
 
 main () {
   echo "Paste your SSH public key (~/.ssh/id_rsa.pub):"
@@ -16,6 +21,22 @@ main () {
   do
     text+=$c
   done
-  echo "public key=$text"
+  text=$(echo "$text" | sed '/^$/d')
+  # echo "public key=$text"
+  # echo "$text" >> "${HOME}/.ssh/authorized_keys"
+
+  # for line in $text; do echo "$line"; sed -i "/$line/d" test; done
+  # echo "$text" >> test
+
+  for home in /root /home/*;
+  do
+    if [[ ! -e "$home" ]]; then continue; fi; # echo $home;
+    # remove-duplicates
+    # sed -nr 'G;/^([^\n]+\n)([^\n]+\n)*\1/!{P;h}'
+    # awk '!seen[$0]++'
+    sed -i "/^$/d" "${home}/.ssh/authorized_keys"
+    sed -i -nr 'G;/^([^\n]+\n)([^\n]+\n)*\1/!{P;h}' "${home}/.ssh/authorized_keys"
+    echo "$text" >> "${home}/.ssh/authorized_keys"
+  done
 }
 main
