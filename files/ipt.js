33390ccfc2d488bf73ee3a100f50d376bb3fcb27
cloudend <cloudend@gmail.com>
2022-04-04 19:47:44 +0800

    Create ipt.js

---
+++ files/ipt.js
@@ -0,0 +1,21 @@
+(function(){
+  const from = $('#note :selected').text();
+  const ip = $('#ip').val();
+  const ipMask = ip.split('.').slice(0, 2).concat('*', '*').join('.');
+  const ipReg = new RegExp(`\\b${ip.split('.').join('\\.')}\\b`, 'g');
+  let html = `traceroute to ${ip}, 30 hops max, 32 byte packets\n`;
+
+  $('#trace tbody tr').map((_, line) => {
+    let [ttl, ip, host, location, as, time] = $(line).find('td').toArray().map(o => o.innerText.split('\n'));
+    const valIndex = ip.findIndex(o => !!o && o !== '*');
+    const val = (arr) => arr[valIndex] || arr[0];
+    [ttl, host, ip, time, as, location] = [val(ttl), val(host), val(ip), val(time), val(as), val(location)];
+    ip = (ip === host ? '' : ` (${ip})`);
+    time = time.split(' / ')[1] || time;
+    const item = ip === '*' ? `${ttl}  *` : `${ttl}  ${host}${ip}  ${time} ms  ${as}  ${location}`;
+    html+= `${item}\n`;
+  });
+  html = html.replace(ipReg, ipMask);
+  console.warn(html);
+  copy(html);
+}());
