c0c3380c1d4853e26b7fc453617923c4381fb4d3
cloudend <cloudend@gmail.com>
2022-05-11 12:56:50 +0800

    Update autoBestTrace.sh

--- autoBestTrace.sh
+++ autoBestTrace.sh
@@ -4,7 +4,10 @@
 # https://github.com/flyzy2005/shell
 
 # besttrace -q1 -T -g cn # 探测数据包数1, TCP, CN
+url="https://raw.githubusercontent.com/nyjx/autoBestTrace/main/autoBestTrace.sh"
+url="https://ghproxy.com/$url" # cdn
 bash <(
-  curl -Lso- https://raw.githubusercontent.com/nyjx/autoBestTrace/main/autoBestTrace.sh \
+  curl -Lso- $url \
+  | sed "s/https:\/\/github.com/https:\/\/ghproxy.com\/https:\/\/github.com/Ig" \
   | sed 's/ -q 1 / -q1 -g cn -T /g'
 )
