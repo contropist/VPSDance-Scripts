78d84ebe302536346f980822462d979d4bdef93b
cloudend <cloudend@gmail.com>
2022-06-01 22:02:59 +0800

    feat: cdn

--- unlockTest.sh
+++ unlockTest.sh
@@ -3,9 +3,11 @@
 # bash <(curl -L -s check.unlock.media)
 # https://github.com/lmc999/RegionRestrictionCheck
 
+url="https://raw.githubusercontent.com/lmc999/RegionRestrictionCheck/main/check.sh"
+url="https://ghproxy.com/$url" # cdn
 main() {
   bash <(
-    curl -L -s https://raw.githubusercontent.com/lmc999/RegionRestrictionCheck/main/check.sh \
+    curl -L -s $url \
     | sed '/^[ \t]*echo\( -e\)\? "[-]*"\(.*\)\?$/d' \
     | sed '/^[ \t]*echo\( -e\)\? "[=]*"\(.*\)\?$/d' \
     | sed 's/ CheckV6().*$/&\n printf "%-39s\\n" \| sed "s\/\\s\/-\/g"/' \
