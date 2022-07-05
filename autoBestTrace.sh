cb41431de52bc915e0d5c17caa1dca4a405d0bbf
cloudend <cloudend@gmail.com>
2022-07-05 09:13:37 +0800

    Update autoBestTrace.sh

--- autoBestTrace.sh
+++ autoBestTrace.sh
@@ -7,6 +7,14 @@
 # besttrace -q1 -T -g cn # 探测数据包数1, TCP, CN
 url="https://raw.githubusercontent.com/nyjx/autoBestTrace/main/autoBestTrace.sh"
 url="https://ghproxy.com/$url" # cdn
+
+# clean faild file
+if [ -f "besttrace2021" ]; then
+  if [[ ! $(./besttrace2021 -V) ]]; then
+    rm -rf besttrace2021;
+  fi
+fi
+
 bash <(
   curl -Lso- $url \
   | sed "s/https:\/\/github.com/https:\/\/ghproxy.com\/https:\/\/github.com/Ig" \
