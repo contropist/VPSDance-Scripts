622cb9dbeef50eceb82438c63afc77b2660da7ca
vps.dance <dancevps@gmail.com>
2022-12-02 13:35:04 +0800

    feat:  for windows

---
+++ win/tool.cmd
@@ -0,0 +1,126 @@
+@ECHO OFF
+:: https://sh.vps.dance/tool.cmd
+:: References: nat.ee, https://t.me/nat_ee
+
+REM GBK/936 UTF-8/65001
+chcp 936>nul
+
+>nul 2>&1 "%SYSTEMROOT%\system32\caCLS.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+GOTO UACPrompt
+) ELSE ( GOTO gotAdmin )
+:UACPrompt
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+ECHO UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+title tool.cmd
+mode con: cols=36 lines=12
+color 17
+SET "wall=HKLM\SYSTEM\ControlSet001\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules"
+SET "rdp=HKLM\SYSTEM\ControlSet001\Control\Terminal Server"
+:Menu
+CLS
+ECHO.
+ECHO 1.修改远程桌面端口
+ECHO.
+ECHO 2.修改用户密码
+ECHO.
+ECHO 3.重启计算机
+ECHO.
+ECHO 4.显示隐藏文件和扩展名
+ECHO.
+choice /C:1234 /N /M "请输入你的选择 [1,2,3,4]": 
+if errorlevel 4 GOTO:ShowHidden
+if errorlevel 3 GOTO:Restart
+if errorlevel 2 GOTO:Password
+if errorlevel 1 GOTO:RemotePort
+:ShowHidden
+CLS
+ECHO 显示隐藏文件和扩展名
+:: Show hidden files
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Hidden /t REG_DWORD /d 1 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowSuperHidden /t REG_DWORD /d 1 /f
+:: Show file extensions
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /f
+taskkill /f /im explorer.exe >nul
+start explorer >nul
+TIMEOUT 3 >NUL
+GOTO:Menu
+:RemotePort
+SET Port=3389
+CLS
+ECHO 修改远程桌面端口
+ECHO.
+ECHO 输入^" q ^"返回主菜单
+ECHO 留空默认使用 3389 端口
+ECHO 按回车键 (Enter) 确定
+ECHO.
+SET /P "Port=自定义端口范围(1-65535):"
+ECHO;%Port%|find " "&&goto:RemotePort
+ECHO;%Port%|findstr "^0.*"&&goto:RemotePort
+IF "%Port%" == "q" (GOTO:Menu)
+IF "%Port%" == "0" (GOTO:RemotePort)
+IF "%Port%" == "" (SET Port=3389)
+IF %Port% LEQ 65535 (
+Reg add "%rdp%\Wds\rdpwd\Tds\tcp" /v "PortNumber" /t REG_DWORD /d "%Port%" /f  > nul
+Reg add "%rdp%\WinStations\RDP-Tcp" /v "PortNumber" /t REG_DWORD /d "%Port%" /f  > NUL
+Reg add "%wall%" /v "{338933891-3389-3389-3389-338933893389}" /t REG_SZ /d "v2.29|Action=Allow|Active=TRUE|Dir=In|Protocol=6|LPort=%Port%|Name=Remote Desktop(TCP-In)|" /f
+Reg add "%wall%" /v "{338933892-3389-3389-3389-338933893389}" /t REG_SZ /d "v2.29|Action=Allow|Active=TRUE|Dir=In|Protocol=17|LPort=%Port%|Name=Remote Desktop(UDP-In)|" /f
+CLS
+ECHO.
+ECHO 修改成功。
+ECHO.
+ECHO 请牢记，你的远程端口是: %Port% 
+ECHO.
+ECHO 重启计算机生效。
+TIMEOUT 5 >NUL
+GOTO:Menu
+) ELSE (
+CLS
+ECHO.
+ECHO 错误端口: %Port% 
+ECHO 大于所设置的范围，
+ECHO 请在^"1 - 65535^"内。
+TIMEOUT 3 >NUL
+GOTO:RemotePort
+)
+:Password
+SET pwd1=
+SET pwd2=
+CLS
+ECHO 修改当前用户: %username% 的密码
+ECHO.
+ECHO 输入^" q ^"返回主菜单
+ECHO 按回车键 (Enter) 确定
+ECHO.
+SET /p pwd1=请输入新密码: 
+IF "%pwd1%" == "q" (GOTO:Menu)
+CLS
+ECHO.
+ECHO 输入^" q ^"返回主菜单
+ECHO 按回车键 (Enter) 确定
+ECHO.
+SET /p pwd2=请再次输入密码: 
+IF "%pwd2%" == "q" (GOTO:Menu)
+IF "%pwd1%" == "%pwd2%" (
+CLS
+net user "%username%" "%pwd2%"||PAUSE&&GOTO:Password
+ECHO.
+TIMEOUT 3 >NUL
+GOTO:Menu
+) ELSE (
+CLS
+ECHO.
+ECHO 密码错误，请重新输入。
+TIMEOUT 3 >NUL
+GOTO:Password
+)
+:Restart
+CLS
+ECHO 正在倒计时重启……
+TIMEOUT /t 5
+shutdown.exe /r /f /t 0
+EXIT
