53ee2ddee617e017c5941f3514e7896c25d5e15c
vps.dance <dancevps@gmail.com>
2023-05-26 09:01:59 +0800

    feat: + debian 12

--- sources.sh
+++ sources.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # bash <(curl -Lso- https://sh.vps.dance/sources.sh) debian11
+# bash <(curl -Lso- https://sh.vps.dance/sources.sh) debian12
 
 sources_list="/etc/apt/sources.list"
 name=$( tr '[:upper:]' '[:lower:]' <<<"$1" )
@@ -17,13 +18,37 @@ echo "deb-src http://deb.debian.org/debian bullseye-backports main"
 ) > $sources_list
 }
 
+debian12() {
+(
+echo "deb http://deb.debian.org/debian bookworm main non-free-firmware"
+echo "deb-src http://deb.debian.org/debian bookworm main non-free-firmware"
+echo "deb http://security.debian.org/debian-security bookworm-security main non-free-firmware"
+echo "deb-src http://security.debian.org/debian-security bookworm-security main non-free-firmware"
+echo "deb http://deb.debian.org/debian bookworm-updates main non-free-firmware"
+echo "deb-src http://deb.debian.org/debian bookworm-updates main non-free-firmware"
+echo "deb http://deb.debian.org/debian bookworm-backports main non-free-firmware"
+echo "deb-src http://deb.debian.org/debian bookworm-backports main non-free-firmware"
+) > $sources_list
+}
+
+tips() {
+  echo "Please run:"
+  echo "apt update -y && apt upgrade -y && apt autoremove -y && apt autoclean -y"
+  echo "/etc/os-release"
+  # apt full-upgrade -y;
+}
+
 main() {
   if [[ "$name" == "debian11" ]]; then
     debian11
+    tips
+  elif [[ "$name" == "debian12" ]]; then
+    debian12
+    tips
   else
     echo "Error: not supported."
     exit 1
   fi
-  echo "Done."
+  # echo "Done."
 }
 main
