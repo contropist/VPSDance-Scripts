77528442324db6ecee0d15a0288f88613a68ae41
vps.dance <dancevps@gmail.com>
2023-08-12 16:49:17 +0800

    feat: fmt

--- checkNf.sh
+++ checkNf.sh
@@ -4,18 +4,19 @@
 # 停止: ps aux | grep checkNf.sh | grep -v grep | awk '{print $2}' | xargs kill -9
 
 interval=2 # 检测间隔(分钟)
-IPv="4" # IPv4 或 IPv6
+IPv="4" # 使用 IPv4 还是 IPv6 检测
 UA_Browser="Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0"
-check_nf() { curl -${IPv}fsL -A "${UA_Browser}" -w %{http_code} -o /dev/null -m 10 "https://www.netflix.com/title/70143836" 2>&1; }
-# echo $(check_nf) # 200: unblock, 404: block
+url="https://www.netflix.com/title/70143836" # Netflix 自制剧URL (絕命毒師)
+check_nf() { curl -${IPv}fsL -A "${UA_Browser}" -w %{http_code} -o /dev/null -m 10 "${url}" 2>&1; }
+# check_nf; # 200: unblock, 404: block
 
-log="/root/ip.txt"
+log="/root/ip.txt" # 日志文件
 while true; do
   code=$(check_nf);
   # echo $code;
   if [ "$code" = "404" ]; then
     date +"%Y-%m-%d %H:%M:%S" >>$log
-    # 调用API自动更换IP
+    # 不解锁时, 调用API自动更换IP
     # curl http://xxx.com/change_ip >>$log
   fi
   sleep $((60 * $interval)) # xx秒
